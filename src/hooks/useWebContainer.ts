53926f2ae2879d6f044cca4860cb1adbf10d7469
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2025-10-11 23:22:05 +0530

    Refactor useWebContainer hook: standardize indentation and formatting for improved readability

--- src/hooks/useWebContainer.ts
+++ src/hooks/useWebContainer.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
-import { WebContainer } from '@webcontainer/api';
+import { WebContainer } from "@webcontainer/api";
 
 export function useWebContainer() {
-    const [webcontainer, setWebcontainer] = useState<WebContainer>();
+  const [webcontainer, setWebcontainer] = useState<WebContainer>();
 
-    async function main() {
-        const webcontainerInstance = await WebContainer.boot();
-        setWebcontainer(webcontainerInstance)
-    }
-    useEffect(() => {
-        main();
-    }, [])
+  async function main() {
+    const webcontainerInstance = await WebContainer.boot();
+    setWebcontainer(webcontainerInstance);
+  }
+  useEffect(() => {
+    main();
+  }, []);
 
-    return webcontainer as WebContainer;
+  return webcontainer as WebContainer;
 }
