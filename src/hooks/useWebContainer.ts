de9934618dffd8ab8df7b88bbf40e0eac871c5cd
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2025-11-18 22:31:14 +0530

    globalWebContainer for not botting multiple times

--- src/hooks/useWebContainer.ts
+++ src/hooks/useWebContainer.ts
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import { WebContainer } from "@webcontainer/api";
 
+let globalWebContainer: WebContainer | null = null;
+
 export function useWebContainer() {
   const [webcontainer, setWebcontainer] = useState<WebContainer>();
 
-  async function main() {
-    const webcontainerInstance = await WebContainer.boot();
-    setWebcontainer(webcontainerInstance);
-  }
   useEffect(() => {
-    main();
+    async function init() {
+      if (!globalWebContainer) {
+        globalWebContainer = await WebContainer.boot();
+      }
+      setWebcontainer(globalWebContainer);
+    }
+
+    init();
   }, []);
 
-  return webcontainer as WebContainer;
+  return webcontainer!;
 }
